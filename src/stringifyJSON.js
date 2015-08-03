9c2c0c06924065b7be827c1829a6b91532f5da3d
Julia Nocera <giulz42@gmail.com>
2015-08-03 15:09:23 -0700

    base and termination cases added plus beginning to work on handling array

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,40 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  
+  //termination cases 
+    //undefined
+  if(obj === undefined){
+    return;
+  }
+    //null
+  if(obj === null){
+    return null;
+  };
+    //function
+  if(typeOf obj === 'function'){
+    return; 
+  }
+//handle string, object, & array
+    
+    //string
+  if(typeOf obj === String){
+    return "'" + obj + "'";
+  } 
+
+    //array
+  if(Array.isArray(obj)){
+    var toReturn = [];
+    if(obj.length > 0){
+      for(var i = 0; i < obj.length; i++){
+       
+
+  
+
+
+
+
+
+
   // your code goes here
 };
