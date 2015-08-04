1a8415de6b725e903304fdb01bae8173f6c2a51a
Julia Nocera <giulz42@gmail.com>
2015-08-04 00:00:33 -0700

    passes both; precourse done

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,27 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ){
+var toReturn = []; //array of elements with className
+	function checkForClass (node){ //traverse doc and find nodes with className
+		if(node.classList){
+			if(node.classList.contains(className)){//if node is found with className --> push to array toReturn
+				toReturn.push(node);
+			}
+		}
+		else{ //no match
+			return;
+		}
+		
+		if(node.childNodes.length > 0){ 
+			for(var i = 0; i < node.childNodes.length; i++){
+				checkForClass(node.childNodes[i]); //recurse
+			}
+		}
+		else{ 
+			return;
+		}
+	}
+checkForClass(document.body); //start at the top of the DOM
+return toReturn;
   // your code here
 };
